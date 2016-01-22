25aa63516213f7d53064bc12537959a1b917be89
am-shopgo <ammar@shopgo.me>
2016-01-22 04:02:29 +0200

    Prevent refreshing disallowed caches

---
+++ Controller/Adminhtml/Cache/MassRefresh.php
@@ -0,0 +1,99 @@
+<?php
+/**
+ *
+ * Copyright © 2015 ShopGo. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+namespace ShopGo\AdvancedAcl\Controller\Adminhtml\Cache;
+
+use Magento\Framework\Exception\LocalizedException;
+use Magento\Framework\Controller\ResultFactory;
+use Magento\Backend\App\Action;
+use ShopGo\AdvancedAcl\Model\Cache\Config as CacheConfig;
+
+class MassRefresh extends \Magento\Backend\Controller\Adminhtml\Cache\MassRefresh
+{
+    /**
+     * @var \ShopGo\AdvancedAcl\Model\Source\DisallowedCache
+     */
+    protected $_disallowedCache;
+
+    /**
+     * @var \ShopGo\AdvancedAcl\Model\Cache\Config
+     */
+    protected $_cacheConfig;
+
+    /**
+     * @param Action\Context $context
+     * @param \Magento\Framework\App\Cache\TypeListInterface $cacheTypeList
+     * @param \Magento\Framework\App\Cache\StateInterface $cacheState
+     * @param \Magento\Framework\App\Cache\Frontend\Pool $cacheFrontendPool
+     * @param \Magento\Framework\View\Result\PageFactory $resultPageFactory
+     * @param \ShopGo\AdvancedAcl\Model\Source\DisallowedCache $disallowedCache
+     * @param CacheConfig $cacheConfig
+     */
+    public function __construct(
+        Action\Context $context,
+        \Magento\Framework\App\Cache\TypeListInterface $cacheTypeList,
+        \Magento\Framework\App\Cache\StateInterface $cacheState,
+        \Magento\Framework\App\Cache\Frontend\Pool $cacheFrontendPool,
+        \Magento\Framework\View\Result\PageFactory $resultPageFactory,
+        \ShopGo\AdvancedAcl\Model\Source\DisallowedCache $disallowedCache,
+        CacheConfig $cacheConfig
+    ) {
+        parent::__construct(
+            $context,
+            $cacheTypeList,
+            $cacheState,
+            $cacheFrontendPool,
+            $resultPageFactory,
+            $disallowedCache
+        );
+
+        $this->_disallowedCache = $disallowedCache->toOptionArray();
+        $this->_cacheConfig = $cacheConfig;
+    }
+
+    /**
+     * Mass action for cache refresh
+     *
+     * @return \Magento\Backend\Model\View\Result\Redirect
+     */
+    public function execute()
+    {
+        try {
+            $types = $this->getRequest()->getParam('types');
+            $updatedTypes = 0;
+            if (!is_array($types)) {
+                $types = [];
+            }
+            $this->_validateTypes($types);
+            foreach ($types as $type) {
+                $access = true;
+
+                if (isset($this->_disallowedCache[$type])) {
+                    $access = $this->_cacheConfig->getCachePageElementAccess([
+                        CacheConfig::DISALLOWED_CACHE_TYPES => [],
+                        CacheConfig::CACHE_TYPE_NODE => ['value' => key($_cache)]
+                    ]);
+                }
+
+                if ($access) {
+                    $this->_cacheTypeList->cleanType($type);
+                    $updatedTypes++;
+                }
+            }
+            if ($updatedTypes > 0) {
+                $this->messageManager->addSuccess(__("%1 cache type(s) refreshed.", $updatedTypes));
+            }
+        } catch (LocalizedException $e) {
+            $this->messageManager->addError($e->getMessage());
+        } catch (\Exception $e) {
+            $this->messageManager->addException($e, __('An error occurred while refreshing cache.'));
+        }
+
+        /** @var \Magento\Backend\Model\View\Result\Redirect $resultRedirect */
+        $resultRedirect = $this->resultFactory->create(ResultFactory::TYPE_REDIRECT);
+        return $resultRedirect->setPath('adminhtml/*');
+    }
+}
